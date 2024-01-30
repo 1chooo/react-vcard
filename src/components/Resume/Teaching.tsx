94bddb81feb902bd15f6051d1658efb3747044c2
1chooo <hugo970217@gmail.com>
2024-01-31 01:09:12 +0800

    update: Resume Teaching

---
+++ src/components/Resume/Teaching.tsx
@@ -0,0 +1,61 @@
+function Teaching() {
+    return (
+        <section className="timeline">
+
+            <div className="title-wrapper">
+                <div className="icon-box">
+                    T
+                </div>
+
+                <h3 className="h3">Teaching</h3>
+            </div>
+
+            <ol className="timeline-list">
+
+                <li className="timeline-item">
+
+                    <h4 className="h4 timeline-item-title">AP4063 Weather and Artificial Intelligence Ⅰ/Ⅱ</h4>
+
+                    <span>Sep, 2023 - Jan, 2024</span>
+
+                    <p className="timeline-text">
+                        <a href="https://www.ncu.edu.tw/" target="_blank" className="code-highlight-yellow">National
+                            Central University</a> | 📍 Taoyuan, Taiwan
+                    </p>
+
+                    <p className="timeline-text">
+                        GitHub: <a href="https://github.com/NCU-GS4719-Python/" target="_blank"
+                            className="code-highlight-yellow">NCU-GS4719-Python
+                        </a> | Course Website: <a href="https://ncuedu.tw/" target="_blank"
+                            className="code-highlight-yellow">NCUEDU
+                        </a>
+                    </p>
+
+                </li>
+
+                <li className="timeline-item">
+
+                    <h4 className="h4 timeline-item-title">AP4063 Weather and Artificial Intelligence Ⅰ/Ⅱ</h4>
+
+                    <span>Sep, 2023 - present</span>
+
+                    <p className="timeline-text">
+                        <a href="https://www.ncu.edu.tw/" target="_blank" className="code-highlight-yellow">National
+                            Central University</a> | 📍 Taoyuan, Taiwan
+                    </p>
+
+                    <p className="timeline-text">
+                        GitHub: <a href="https://github.com/weather-and-ai" target="_blank"
+                            className="code-highlight-yellow">Weather and Artificial Intelligence
+                        </a>
+                    </p>
+
+                </li>
+
+            </ol>
+
+        </section>
+    );
+}
+
+export default Teaching;
